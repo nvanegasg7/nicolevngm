65aa2d2687cd3d45df82cb3bb49c95a2deb37daf
nvanegasg7 <77794372+nvanegasg7@users.noreply.github.com>
2021-11-04 12:48:33 -0400

    Add files via upload

---
+++ examenfinaltecno.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+#include <string>
+#include <windows.h>
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+main(){
+	int sobre,plato,pos,pro,devu,suma,factu,dine;
+	cout<<"cuanto cuesta la sobremesa?";
+ cin>>sobre;
+ cout<<"cuanto cuesta el plato fuerte?";
+ cin>>plato;
+ cout<<"cuanto cuesta el postre";
+ cin>>pos;
+ cout<<"cuanto dinero va a dar de propina?";
+ cin>>pro;
+suma=sobre+plato+pos+pro+devu;
+cout<<"su total a pagar es";
+ cin>>factu;
+ cout<<"con cuanto dinero va a pagar?";
+ cin>>dine;
+}
